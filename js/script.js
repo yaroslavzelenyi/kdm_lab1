c82700b931138af76ea107aca1f062ab12f334a4
Yaroslav Zelenyi <yaroslagreen@gmail.com>
2021-09-07 18:40:55 +0300

    five

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-    {id: 1, label: 'x1'},
-    {id: 2, label: 'x2'},
-    {id: 3, label: 'x3'},
-    {id: 4, label: 'x4'},
-    {id: 5, label: 'x5'}
+    { id: 1, label: 'x1' },
+    { id: 2, label: 'x2' },
+    { id: 3, label: 'x3' },
+    { id: 4, label: 'x4' },
+    { id: 5, label: 'x5' },
 ]);
 
 // create an array with edges
 var edges = new vis.DataSet([
-    {from: 1, to: 2, arrows: 'to'},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5},
-    {from: 3, to: 4, arrows: 'to'},
-    {from: 5, to: 5}
+    { from: 1, to: 2, arrows: 'to' },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 4, arrows: 'to' },
+    { from: 5, to: 5 },
 ]);
 
 // create a network
@@ -23,18 +23,18 @@ var container = document.getElementById('mynetwork');
 // provide the data in the vis format
 var data = {
     nodes: nodes,
-    edges: edges
+    edges: edges,
 };
 var options = {
     manipulation: {
         enabled: true,
         addEdge: function (edgeData, callback) {
-            if(graphType) {
+            if (graphType) {
                 edgeData.arrows = 'to';
             }
             callback(edgeData);
-        }
-    }
+        },
+    },
 };
 
 // initialize your network!
--- js/switch.js
+++ js/switch.js
@@ -1,130 +1,142 @@
-'use strict'
-window.addEventListener('DOMContentLoaded', ()=>{
-
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
     const wrapper = document.querySelector('.switcher__wrapper'),
-          switcher = document.querySelectorAll('.switcher__item'),
-          switchBlock = document.querySelector('.switcher__color'),
-          buttonWrapper = document.querySelector('.table__buttons'),
-          btns = document.querySelectorAll('.arrow'),
-          right = document.querySelector('.right'),
-          left = document.querySelector('.left'),
-          clear = document.querySelector('.clear'),
-          build = document.querySelector('.build'),
-          graphBtns = document.querySelectorAll('.build-buttons__item'),
-          tblBtnAdd = document.querySelector('.plus'),
-          tblBtnRemove = document.querySelector('.minus'),
-          tableA = document.querySelector('#tableA'),
-          tableB = document.querySelector('#tableB'),
-          tr = document.getElementsByTagName('tr'),
-          close = document.querySelector('.close'),
-          modal = document.querySelector('.modal');
-          let rowCount;
+        switcher = document.querySelectorAll('.switcher__item'),
+        switchBlock = document.querySelector('.switcher__color'),
+        buttonWrapper = document.querySelector('.table__buttons'),
+        btns = document.querySelectorAll('.arrow'),
+        right = document.querySelector('.right'),
+        left = document.querySelector('.left'),
+        clear = document.querySelector('.clear'),
+        build = document.querySelector('.build'),
+        graphBtns = document.querySelectorAll('.build-buttons__item'),
+        tblBtnAdd = document.querySelector('.plus'),
+        tblBtnRemove = document.querySelector('.minus'),
+        tableA = document.querySelector('#tableA'),
+        tableB = document.querySelector('#tableB'),
+        tr = document.getElementsByTagName('tr'),
+        close = document.querySelector('.close'),
+        input = document.querySelectorAll('input'),
+        modal = document.querySelector('.modal');
+    let rowCount;
 
-
-    wrapper.addEventListener('click', (event)=>{
-        
+    wrapper.addEventListener('click', (event) => {
         let target = event.target;
-        if(target.classList.contains('switcher__item')){
-            if(!(target.classList.contains('active'))){
-                switcher.forEach((g)=>{
+        if (target.classList.contains('switcher__item')) {
+            if (!target.classList.contains('active')) {
+                switcher.forEach((g) => {
                     g.classList.toggle('active');
                 });
-
             }
-            if(!(target.classList.contains('forward'))){
-                switchBlock.style.cssText += 'transform: translateX(120px);transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)';
-            } else{
-                switchBlock.style.cssText += 'transform: translateX(0);transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)';
+            if (!target.classList.contains('forward')) {
+                switchBlock.style.cssText +=
+                    'transform: translateX(120px);transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)';
+            } else {
+                switchBlock.style.cssText +=
+                    'transform: translateX(0);transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1)';
             }
         }
     });
-    function btnAnimate(array,activeCLass, first, second){
-        array.forEach((btn)=>{
-            btn.addEventListener('click',()=>{
+    function btnAnimate(array, activeCLass, first, second) {
+        array.forEach((btn) => {
+            btn.addEventListener('click', () => {
                 btn.classList.toggle(activeCLass);
-                if(btn.classList.contains(first, activeCLass)){
-
+                if (btn.classList.contains(first, activeCLass)) {
                     array[1].classList.remove(activeCLass);
                 }
-                if(btn.classList.contains(second, activeCLass)){
-                    
+                if (btn.classList.contains(second, activeCLass)) {
                     array[0].classList.remove(activeCLass);
                 }
-
             });
         });
     }
-    tblBtnAdd.addEventListener('click', function building(){
+    tblBtnAdd.addEventListener('click', function building() {
         for (let i = 0; i < tr.length; i++) {
-
             let td = document.createElement('td');
             let input = document.createElement('INPUT');
-            input.value='0';
+            input.value = '0';
             input.classList = 'table__input';
             td.appendChild(input);
             tr[i].appendChild(td);
-            
         }
-        
-        function buildRowTable(table){
 
+        function buildRowTable(table) {
             let newRow = table.insertRow();
             rowCount = $('#tableA tr').length;
 
-            for(let i = 0; i < rowCount; i++){
-                
+            for (let i = 0; i < rowCount; i++) {
                 let td = document.createElement('td');
                 let input = document.createElement('INPUT');
-                input.value='0';
+                input.value = '0';
                 input.classList = 'table__input';
                 td.appendChild(input);
                 newRow.appendChild(td);
-                
             }
-            if(rowCount === 8){
-                tblBtnAdd.removeEventListener('click',  building);
+            if (rowCount === 8) {
+                tblBtnAdd.removeEventListener('click', building);
             }
         }
         buildRowTable(tableA);
         buildRowTable(tableB);
-        
     });
-    tblBtnRemove.addEventListener('click', ()=>{
-        removeColumn();
+    tblBtnRemove.addEventListener('click', () => {
+        removeColumn(tableA);
+        removeColumn(tableB);
         removeRow(tableA);
         removeRow(tableB);
     });
-    function removeColumn(){
+    function removeColumn(table) {
         let td = document.querySelectorAll('td');
         let rowLength = $('#tableA tr').length;
         for (let i = 0; i < rowLength; i++) {
-
-            tr[i].lastElementChild.remove();
-            
+            table.rows[i].lastElementChild.remove();
         }
     }
-    function removeRow(table){
+    function removeRow(table) {
         const tableName = table;
         let rowLength = $('#tableA tr').length;
-        // tr[--rowLength].remove();
         tableName.deleteRow(--rowLength);
     }
 
-    build.addEventListener('click',() =>{
+    build.addEventListener('click', () => {
         modal.style.top = '0';
     });
-    close.addEventListener('click', () =>{
+    close.addEventListener('click', () => {
         modal.style.top = '-110%';
     });
-    window.addEventListener('click',(event)=>{
-        if(event.target == modal){
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
             modal.style.top = '-110%';
         }
     });
+    clear.addEventListener('click', () => {
+        input.forEach((item) => {
+            item.value = '0';
+        });
+    });
+    input.forEach((element) => {
+        element.addEventListener('click', () => {
+            element.value = '';
+        });
+    });
+    window.addEventListener('click', (event) => {
+        let target = event.target;
+        if (!target.classList.contains('table__input')) {
+            input.forEach((item) => {
+                if (item.value == '') {
+                    item.value = '0';
+                }
+            });
+            // console.log(target);
+        }
+    });
+    // tableA.addEventListener('click', (event) => {
+    //     let target = event.target;
+    //     if ((target.classList.contains = 'table__input')) {
+    //         input.forEach((el) => {
+    //             el.value = '';
+    //         });
+    //     }
+    // });
     btnAnimate(btns, 'button__active', 'right', 'left');
-    // btnAnimate(graphBtns, 'build-buttons__active', 'build', 'clear');
-    
-   
-    
 });
-
